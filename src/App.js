beeff9f91235edb5b51197cc220e6b2be14a59a8
scKim <lansco1229@gmail.com>
2024-11-13 22:43:46 +0900

    feat: add a product page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ChatbotCreation from "./pages/ChatbotPage/ChatbotPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
 import AppRouter from "./routes/AppRouter";
+
 function App() {
+  
   return (
     <div className="App">
+
       {/* <ChatbotCreation /> */}
-      <AppRouter />
+      {/* <AppRouter /> */}
+      <ProductPage />
+
     </div>
+    
   );
 }
 
