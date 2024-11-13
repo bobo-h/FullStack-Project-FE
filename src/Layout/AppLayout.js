90b5987cdaf0bb3bafa0b3238216a0c029c18ed9
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-11-13 23:24:41 +0900

    오타 수정

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Navbar from "../common/components/Navbar";
+import Navbar from "../common/components/NavBar";
 import "./style/applayout.style.css";
 import LodingSpinner from "../common/components/LodingSpinner";
 
