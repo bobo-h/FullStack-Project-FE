1878e27a7ac04904056566b0f18b91c55c17b7dc
Dahee Park <dahee5654@gmail.com>
2024-11-14 17:58:23 +0900

    docs : applayout.style 문서 수정  justify-content / align-items : center -> flex-start 로 일시적 수정
     feat : add UserInfoPage

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -7,13 +7,35 @@ import "./style/sidebarForMyPage.style.css";
 const MyPage = () => {
   return (
     <div>
-      <Container className="mypage-style">
-        <Row className="vh-100">
-          <Col xs={12} md={3} className="sidebar mobile-sidebar">
+      <Container fluid className="mypage-style">
+        <Row>
+          {/* 사이드바: 화면 높이에 고정 */}
+          <Col xs={12} md={3} className="sidebar mobile-sidebar position-fixed">
             <SidebarForMyPage />
           </Col>
-          <Col xs={12} md={9} className="content">
-            <UserInfoPage />
+
+          {/* 콘텐츠: 사이드바 옆으로 오프셋 적용 */}
+          <Col xs={12} md={{ span: 9, offset: 3 }} className="userInfo-area">
+            {/* 유저 정보 섹션 */}
+            <Row className="mt-4">
+              <Col>
+                <UserInfoPage />
+              </Col>
+            </Row>
+
+            {/* 추가 콘텐츠 섹션 */}
+            <Row className="mt-5">
+              <Col>
+                <div className="additional-content">
+                  {/* 여기에 새로운 콘텐츠 컴포넌트를 추가할 수 있습니다 */}
+                  <h2>추가 콘텐츠</h2>
+                  <p>
+                    이곳에 유저의 다른 정보를 표시하거나 다른 기능을 추가할 수
+                    있습니다.
+                  </p>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
