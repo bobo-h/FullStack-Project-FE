9301aad5e6f08d78a47347dfa98117e07d97f6aa
Dahee Park <dahee5654@gmail.com>
2024-11-14 20:12:45 +0900

    docs : applaylout.style.css 다시 돌려 놓기 (flex-start -> center)
    feat: 수빈님이 공유해주신 applayout 적용안되는 거 my-page에 적용

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import NavBar from "../common/components/NavBar";
 import "./style/applayout.style.css";
 import LodingSpinner from "../common/components/LodingSpinner";
 
 const AppLayout = ({ children }) => {
+  const location = useLocation();
   useEffect(() => {}, []);
   const loging = false;
 
+  // 현재 경로가 /my-page인지 확인
+  const isMyPage = location.pathname === "/my-page";
+  // 경로가 /my-page일 때 display: flex를 제거하려면 no-flex 클래스 추가
+  const appLayoutClass = isMyPage ? "no-flex" : "display-flex";
+
   return (
-    <div className="app-layout">
+    <div className={`.app-layout ${appLayoutClass}`}>
       <NavBar />
       {loging ? <LodingSpinner /> : children}
     </div>
